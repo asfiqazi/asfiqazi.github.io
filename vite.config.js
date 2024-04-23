16df79d731addc2e4768d25e272260f1dde1bfa1
asfigit <demouserasfi@gmail.com>
2024-04-23 08:38:23 +0500

    Deleted React App

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import reactRefresh from '@vitejs/plugin-react-refresh';
-
-export default defineConfig({
-  plugins: [react()],
-  
-})
